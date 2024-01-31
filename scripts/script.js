662096cedf70edf112b20eb9ea0d6625dc44dffb
Kseniia Telegina <ksusha2993@gmail.com>
2024-01-31 12:05:03 +0200

    разработка на 31.01.24

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,5 @@
 function onClickMenu(){
  document.getElementById("btn-menu").classList.toggle("active");
  document.getElementById("ul-menu").classList.toggle("active");
+ document.getElementById("btn-image").classList.toggle("active");
 }
