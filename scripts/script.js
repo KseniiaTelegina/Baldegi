d4b870c921599f6cead8e2f38ac350e6416dcf75
Kseniia Telegina <ksusha2993@gmail.com>
2024-01-31 18:53:10 +0200

    разработка на вечер 31.01.24

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,27 @@ function onClickMenu(){
  document.getElementById("ul-menu").classList.toggle("active");
  document.getElementById("btn-image").classList.toggle("active");
 }
+
+// let swiper = new Swiper(".mySwiper-masters", {
+//     slidesPerView: 1,
+//     spaceBetween: 7.5,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       color: white,
+//     },
+//     breakpoints: {
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 7.5,
+//       },
+//       768: {
+//         slidesPerView: 4,
+//         spaceBetween: 7.5,
+//       },
+//       1024: {
+//         slidesPerView: 6,
+//         spaceBetween: 7.5,
+//       },
+//     },
+//   });
