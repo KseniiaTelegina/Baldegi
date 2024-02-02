26dcd05c5a8df82d5f5141dbf48f1ed6ea93184d
Kseniia Telegina <ksusha2993@gmail.com>
2024-02-02 11:19:51 +0200

    разработка на 02.02.24

--- scripts/script.js
+++ scripts/script.js
@@ -30,10 +30,30 @@ function onClickMenu(){
 //   });
 
 new Swiper(".mySwiper-masters", {
+    slidesPerView: 1,
+    spaceBetween: 7.5,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
         dynamicBullets: true,
+    },
+    breakpoints: {
+        481: {
+        slidesPerView: 2,
+        spaceBetween: 7.5,
+        },
+        620: {
+            slidesPerView: 3,
+            spaceBetween: 7.5,
+            },
+        768: {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+            }
     },
 });
 
@@ -43,4 +63,10 @@ new Swiper(".mySwiper-reviews", {
         clickable: true,
         dynamicBullets: true,
     },
+    breakpoints: {
+        768: {
+        slidesPerView: 'auto',
+        spaceBetween: 7.5,
+        }
+    }
 });
